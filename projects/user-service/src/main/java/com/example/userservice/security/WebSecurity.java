dd954847c45e5a7d693f20c8ec1d75f3ad1f0c82
gumicode <gumicode@gmail.com>
2021-07-03 00:46:29 +0900

    add project config-service

--- projects/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ projects/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -23,9 +23,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
-
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
 //        http.authorizeRequests().antMatchers("/**").hasIpAddress("192.168.1.2");
-
         http.addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable();
     }
